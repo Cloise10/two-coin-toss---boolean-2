ba06b2e8c7b7e3074d0a08a59eaea0a036c198a3
Luna Nyx <79994302+Cloise10@users.noreply.github.com>
2021-03-09 13:35:57 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    boolean = true || false
+    if (true) {
+        basic.showString("heads")
+        basic.showIcon(IconNames.Ghost)
+    } else if (false) {
+        basic.showString("tails")
+        basic.showIcon(IconNames.Sword)
+    }
+    basic.pause(200)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+let boolean = false
+basic.showString("Coin Toss")
+basic.forever(function () {
+	
+})
